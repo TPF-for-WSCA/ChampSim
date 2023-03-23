65034850ca31db778b0e86af320b623138ce6b08
Roman K. Brunner <roman.k.brunner@ntnu.no>
2023-03-23 15:07:32 +0100

    runner updates

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-build_configs=("saga_8k_config.json" "saga_64k_config.json" "saga_4k_config.json" "saga_32k_config.json" "saga_2k_config.json" "saga_1k_config.json" "saga_16k_config.json" "saga_128k_config.json")
+build_configs=("saga_8k_config.json" "saga_64k_config.json" "saga_4k_config.json" "saga_32k_config.json" "saga_2k_config.json" "saga_1k_config.json" "saga_16k_config.json" "saga_128k_config.json" "saga_bigcache_config.json")
 
+old_dir=pwd
+cd ~/workspace/ChampSim/
 for build_script in ${build_configs[@]}
 do
-    ./config.sh $build_script
+    echo "Building ${build_script}"
+    ~/workspace/ChampSim/config.sh ~/workspace/ChampSim/${build_script}
     make
 done
+cd $old_dir
