f935fe09eeaecd892dfd829a2e050499764f37a1
Roman Kaspar Brunner <roman.k.brunner@ntnu.no>
2023-03-24 13:24:29 +0100

    Aggregation script extended and larger vcl

--- aggregation_scripts/ipc_data.py
+++ aggregation_scripts/ipc_data.py
@@ -3,50 +3,99 @@ import os
 import re
 import sys
 from collections import defaultdict
+from enum import Enum
 
-ipc_by_cachesize_and_workload = defaultdict((lambda: defaultdict(int)))
-out_dir=sys.argv[1]
-for subdir in os.listdir(out_dir):
-    if not os.path.isdir(
-        os.path.join(out_dir, subdir)
-    ):
-        continue
-    size = subdir.split("champsim")[2]
-    matches = re.match(r"(\d+)([km])", size).groups()
-    factor = 1024 if matches[1] == "k" else 1024*1024
-    size_bytes = int(matches[0]) * factor
-    for workload in os.listdir(
-        f"{out_dir}/{subdir}"
-    ):
-        for logfile in os.listdir(
-            f"{out_dir}/{subdir}/{workload}"
-        ):
+
+class STATS(Enum):
+    IPC = 1
+    MPKI = 2
+
+
+type = STATS.IPC
+
+
+def extract_ipc(path):
+    logs = []
+    with open(path) as f:
+        logs = f.readlines()
+    regex = re.compile("CPU 0 cumulative IPC\: (\d*\.?\d+)")
+    logs.reverse()
+    for line in logs:  # reverse to find last run first
+        matches = regex.match(line)
+        if matches:
+            return matches.groups()[0]
+
+
+def extract_l1i_mpki(path):
+    logs = []
+    with open(path) as f:
+        logs = f.readlines()
+    regex = re.compile("cpu0\_L1I MPKI\: (\d*\.?\d+)")
+    logs.reverse()
+    for line in logs:  # reverse to find last run first
+        matches = regex.match(line)
+        if matches:
+            return matches.groups()[0]
+
+
+def single_run(path):
+    stat_by_workload = {}
+    for workload in os.listdir(path):
+        if not os.path.isdir(os.path.join(path, workload)):
+            continue  # if we do this for a single single run
+        for logfile in os.listdir(f"{path}/{workload}"):
             if not ".txt" in logfile:
                 continue
+            if type == STATS.MPKI:
+                stat_by_workload[workload] = extract_l1i_mpki(
+                    f"{path}/{workload}/{logfile}"
+                )
+            else:
+                stat_by_workload[workload] = extract_ipc(
+                    f"{path}/{workload}/{logfile}"
+                )
+    return stat_by_workload
 
-            logs = []
-            with open(
-                f"{out_dir}/{subdir}/{workload}/{logfile}"
-            ) as f:
-                logs = f.readlines()
-            regex = re.compile("CPU 0 cumulative IPC\: ([+-]?\d*\.?\d+)")
-            for line in logs:
-                matches = regex.match(line)
-                if matches:
-                    ipc_by_cachesize_and_workload[size_bytes][
-                        workload
-                    ] = matches.groups()[0]
 
-header = False
-with open("./ipc.tsv", "w+") as outfile:
-    for csize, values in ipc_by_cachesize_and_workload.items():
-        if not header:
-            header = True
-            for workload, ipc in ipc_by_cachesize_and_workload[csize].items():
-                outfile.write(f"\t{workload}")
+def mutliple_sizes_run():
+    ipc_by_cachesize_and_workload = {}
+    out_dir = sys.argv[1]
+    for subdir in os.listdir(out_dir):
+        if not os.path.isdir(os.path.join(out_dir, subdir)):
+            continue
+        size = subdir.split("champsim")[2]
+        matches = re.match(r"(\d+)([km])", size).groups()
+        factor = 1024 if matches[1] == "k" else 1024 * 1024
+        size_bytes = int(matches[0]) * factor
+        ipc_by_cachesize_and_workload[size_bytes] = single_run(
+            f"{out_dir}/{subdir}"
+        )
+    return ipc_by_cachesize_and_workload
+
+
+def write_tsv(data):
+    header = False
+    filename = "./ipc.tsv" if type == STATS.IPC else "./mpki.tsv"
+    with open(filename, "w+") as outfile:
+        for csize, values in data.items():
+            if not header:
+                header = True
+                for workload, ipc in data[csize].items():
+                    outfile.write(f"\t{workload}")
+                outfile.write("\n")
+
+            outfile.write(f"{csize}")
+            for workload, ipc in data[csize].items():
+                outfile.write(f"\t{ipc}")
             outfile.write("\n")
 
-        outfile.write(f"{csize}")
-        for workload, ipc in ipc_by_cachesize_and_workload[csize].items():
-            outfile.write(f"\t{ipc}")
-        outfile.write("\n")
+
+data = {}
+if sys.argv[3] == "MPKI":
+    type = STATS.MPKI
+if sys.argv[2] == "single":
+    data["const"] = single_run(sys.argv[1])
+else:
+    data = mutliple_sizes_run()
+
+write_tsv(data)
