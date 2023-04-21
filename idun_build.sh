361254eb7a4f113b358108e1e5e0fbefc0840629
Roman Kaspar Brunner <roman.k.brunner@ntnu.no>
2023-04-21 13:04:18 +0200

    Got thingssorted

---
+++ idun_build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+build_configs=("idun_8k_config.json" "idun_64k_config.json" "idun_4k_config.json" "idun_32k_config.json" "idun_2k_config.json" "idun_1k_config.json" "idun_16k_config.json" "idun_128k_config.json" "idun_bigcache_config.json")
+
+old_dir=pwd
+cd ~/ChampSim/
+for build_script in ${build_configs[@]}
+do
+    echo "Building ${build_script}"
+    ~/ChampSim/config.sh ~/ChampSim/${build_script}
+    make
+done
+cd $old_dir
