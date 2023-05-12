19ddb799c03ed9e81b7e097c3bb0a4bef86eb372
Roman Kaspar Brunner <roman.k.brunner@ntnu.no>
2023-05-12 15:00:21 +0200

    enable build for buffer config

--- idun_build.sh
+++ idun_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-build_configs=("idun_8k_config.json" "idun_64k_config.json" "idun_4k_config.json" "idun_32k_config.json" "idun_2k_config.json" "idun_1k_config.json" "idun_16k_config.json" "idun_128k_config.json" "idun_bigcache_config.json")
+build_configs=("idun_8k_config.json" "idun_64k_config.json" "idun_4k_config.json" "idun_32k_config.json" "idun_2k_config.json" "idun_1k_config.json" "idun_16k_config.json" "idun_128k_config.json" "idun_bigcache_config.json" "idun_vcl_buffer_config.json")
 
 old_dir=pwd
 cd ~/ChampSim/
