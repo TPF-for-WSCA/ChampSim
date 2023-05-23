3d4d1f4ce852a8ee17103f6393dc9b4e895ff0d0
Roman Kaspar Brunner <roman.k.brunner@ntnu.no>
2023-05-23 17:18:19 +0200

    add more buffer entries

---
+++ aggregation_scripts/ways_handled.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+import os
+import re
+import sys
+from collections import defaultdict
+from enum import Enum
+
+
+class STATS(Enum):
+    IPC = 1
+    MPKI = 2
+
+
+type = STATS.IPC
+
+
+def extract_linex_handled_by_way(path):
+    logs = []
+    with open(path) as f:
+        logs = f.readlines()
+    regex_title = re.compile("cpu0\_L1I LINES HANDLED PER WAY\n")
+    regex_other_title = re.compile("cpu0\_[a-zA-Z]+ LINES HANDLED PER WAY\n")
+    logs.reverse()
+    lines_per_way = []
+    collect_lines = False
+    for line in logs:  # reverse to find last run first
+        matches = regex_title.match(line)
+        if matches:
+            lines_per_way.reverse()
+            return lines_per_way
+        matches = regex_other_title.match(line)
+        if matches:
+            lines_per_way = []
+            collect_lines = True
+            continue
+        if collect_lines:
+            num_cl = int(line.strip().split(":")[1].strip())
+            lines_per_way.append(num_cl)
+
+
+def single_run(path):
+    stat_by_workload = {}
+    for workload in os.listdir(path):
+        if (
+            not os.path.isdir(os.path.join(path, workload))
+            or workload == "graphs"
+            or workload == "raw_data"
+        ):
+            continue  # if we do this for a single single run
+        for logfile in os.listdir(f"{path}/{workload}"):
+            if not ".txt" in logfile:
+                continue
+            stat_by_workload[workload] = extract_linex_handled_by_way(
+                f"{path}/{workload}/{logfile}"
+            )
+    return stat_by_workload
+
+
+def write_tsv(data):
+    filename = "./ways_handled.tsv"
+    with open(filename, "w+") as outfile:
+        for wload, values in data.items():
+            if values is None:
+                continue
+            outfile.write(f"{wload}")
+            for lines in values:
+                outfile.write(f"\t{lines}")
+            outfile.write("\n")
+
+
+data = {}
+data = single_run(sys.argv[1])
+write_tsv(data)
