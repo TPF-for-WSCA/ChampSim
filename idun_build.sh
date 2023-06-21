c37a0d232ceda142703ccbaa9848f358cda193b1
Roman K. Brunner <roman.k.brunner@ntnu.no>
2023-06-21 14:33:25 +0200

    update build pipeline

--- idun_build.sh
+++ idun_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #build_configs=("idun_vcl_buffer_64d_max_way_config.json")
-build_configs=("idun_4k_config.json" "idun_8k_config.json" "idun_16k_config.json" "idun_32k_config.json" "idun_4k_data_config.json" "idun_8k_data_config.json" "idun_16k_data_config.json" "idun_32k_data_config.json" "idun_64k_data_config.json" "idun_128k_data_config.json" "idun_256k_data_config.json" "idun_vcl_buffer_8f_config.json" "idun_vcl_buffer_4f_config.json" "idun_vcl_buffer_128f_config.json" "idun_vcl_buffer_64f_config.json" "idun_vcl_buffer_32f_config.json" "idun_vcl_buffer_16f_config.json" "idun_vcl_buffer_256f_config.json" "idun_vcl_buffer_64d_max_way_config.json") 
+build_configs=("idun_4k_config.json" "idun_8k_config.json" "idun_16k_config.json" "idun_32k_config.json" "idun_64k_config.json" "idun_128k_config.json" "idun_256k_config.json" "idun_4k_data_config.json" "idun_8k_data_config.json" "idun_16k_data_config.json" "idun_32k_data_config.json" "idun_64k_data_config.json" "idun_128k_data_config.json" "idun_256k_data_config.json" "idun_vcl_buffer_8f_config.json" "idun_vcl_buffer_4f_config.json" "idun_vcl_buffer_128f_config.json" "idun_vcl_buffer_64f_config.json" "idun_vcl_buffer_32f_config.json" "idun_vcl_buffer_16f_config.json" "idun_vcl_buffer_256f_config.json" "idun_vcl_buffer_64d_max_way_config.json") 
 
 old_dir=pwd
 cd ~/ChampSim/
