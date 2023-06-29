7c14baa2435ce6329bfc498c1e98b22dcf4fdbb4
Roman Kaspar Brunner <roman.k.brunner@ntnu.no>
2023-06-29 17:09:36 +0200

    not loosing anything here...

--- aggregation_scripts/graphing.sh
+++ aggregation_scripts/graphing.sh
@@ -3,3 +3,14 @@ export filename=ipc.tsv; export output_name='ipc_avg_bar'; plotgen -i ./${filena
 
 # Block and similar stats
 export filename=num_cl_with_block_size.tsv; export output_name='block_sizes_accum_avg_bar'; plotgen -i */${filename} --join index --no-columns --icolumn-names 1: --trace-names */${filename} --palette bright --add-column AVG --apply-func add --apply-icolumns :7 7: --apply-func cumsum --apply-columns : --print --normalise-function max --normalise-rows --print --plot bar --line-mode lines+markers --file ./raw_data/${output_name}.tsv -o ./graphs/${output_name}.html
+
+# Some sort of naming but naming is somehow wrong when executed from bash
+ export filename='cpu0_L1I_buffer_num_cl_with_block_size.tsv'; export
+ output_name='block_sizes_buffer_accum_avg_bar'; plotgen -i
+ ./**/sizes_champsim64k/**/cpu0_L1I_num_cl_with_block_size.tsv -i
+ ./**/sizes_champsim32k/**/cpu0_L1I_num_cl_with_block_size.tsv -i
+ ./**/sizes_champsim_vcl_buffer_*/**/cpu0_L1I_buffer_num_cl_with_block_size.tsv --no-columns --join
+ index --icolumn-names 0: ./**/sizes_champsim64k/**/cpu0_L1I_num_cl_with_block_size.tsv
+ ./**/sizes_champsim32k/**/cpu0_L1I_num_cl_with_block_size.tsv $(realpath
+ ./**/sizes_champsim_vcl_buffer_*
+ /**/cpu0_L1I_buffer_num_cl_with_block_size.tsv | cut -d"/" -f9-11 -) --print
