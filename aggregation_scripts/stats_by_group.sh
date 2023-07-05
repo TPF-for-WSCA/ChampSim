e51ad21c1d9bb2d6079420bbd667baf06a073260
Roman K. Brunner <roman.k.brunner@ntnu.no>
2023-07-05 09:40:18 +0200

    partial commit

--- aggregation_scripts/stats_by_group.sh
+++ aggregation_scripts/stats_by_group.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 benchmarks=("tanvir", "ipc1_spec", "ipc1_server", "ipc1_client")
+
+for b in ${benchmarks[@]}
+do
+	echo "Accumulating ${b}"
+	python ~/ChampSim/aggregation_scripts/ipc_data.py ./${b} multi MPKI
+	python ~/ChampSim/aggregation_scripts/ipc_data.py ./${b} multi IPC
+	python ~/ChampSim/aggregation_scripts/ipc_data.py ./${b} multi PARTIAL
+
+	echo "Plotting ${b}"
+	plotgen -i mpki.tsv --palette bright --add-column AVG --apply-func add --apply-icolumns : 
+done
