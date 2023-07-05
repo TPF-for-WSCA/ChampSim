e0018a563964b4db34904e84de376afc619ce499
Roman Kaspar Brunner <roman.k.brunner@ntnu.no>
2023-07-05 11:53:49 +0200

    add chroot

--- aggregation_scripts/stats_by_group.sh
+++ aggregation_scripts/stats_by_group.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
 benchmarks=("tanvir" "ipc1_spec" "ipc1_server" "ipc1_client")
+pg_dir=""
+
+if [ -z ${plotgen_dir+x} ]; then pg_dir=""; else pg_dir=$plotgen_dir; fi
+
+echo ${pg_dir}
+
+
+chroot=""
+
+if [ -z ${champsim_root+x} ]; then chroot=""; else chroot=$champsim_root; fi
+
+echo ${chroot}
 
 for b in ${benchmarks[@]}
 do
 	echo "Accumulating ${b}"
     cd ./${b}
-	python ~/Workspace/Simulators/ChampSim/aggregation_scripts/ipc_data.py ./ multi MPKI
-	python ~/Workspace/Simulators/ChampSim/aggregation_scripts/ipc_data.py ./ multi IPC
-	python ~/Workspace/Simulators/ChampSim/aggregation_scripts/ipc_data.py ./ multi PARTIAL
+	python ${chroot}/ChampSim/aggregation_scripts/ipc_data.py ./ multi MPKI
+	python ${chroot}/ChampSim/aggregation_scripts/ipc_data.py ./ multi IPC
+	python ${chroot}/ChampSim/aggregation_scripts/ipc_data.py ./ multi PARTIAL
     cd ..
 
 	echo "Plotting ${b}"
     
-    plotgen --debug -i ./${b}/mpki.tsv --drop-nan --palette bright --add-column AVG --apply-func add --apply-icolumns :-1 --normalise-to-row $((2**15)) --apply-func sub 1 --apply-icolumns : --x-type category --y-tick-format ',.2%' --plot bar --file ./raw_data/mpki_relative_${b}.tsv -o ./graphs/mpki_relative_${b}.html
-    plotgen --debug -i ./${b}/ipc.tsv --drop-nan --palette bright --add-column AVG --apply-func add --apply-icolumns :-1 --normalise-to-row $((2**15)) --apply-func sub 1 --apply-icolumns : --x-type category --y-tick-format ',.2%' --plot bar --file ./raw_data/ipc_relative_${b}.tsv -o ./graphs/ipc_relative_${b}.html
-    plotgen --debug -i ./${b}/partial.tsv --drop-nan --apply-func div 100 --apply-icolumns : --palette bright --x-type category --y-tick-format ',.2%' --plot bar --file ./raw_data/partial_relative_${b}.tsv -o ./graphs/partial_relative_${b}.html
+    ${pg_dir}plotgen --debug -i ./${b}/mpki.tsv --drop-nan --palette bright --add-column AVG --apply-func add --apply-icolumns :-1 --normalise-to-row $((2**15)) --apply-func sub 1 --apply-icolumns : --x-type category --y-tick-format ',.2%' --plot bar --file ./raw_data/mpki_relative_${b}.tsv -o ./graphs/mpki_relative_${b}.html
+    ${pg_dir}plotgen --debug -i ./${b}/ipc.tsv --drop-nan --palette bright --add-column AVG --apply-func add --apply-icolumns :-1 --normalise-to-row $((2**15)) --apply-func sub 1 --apply-icolumns : --x-type category --y-tick-format ',.2%' --plot bar --file ./raw_data/ipc_relative_${b}.tsv -o ./graphs/ipc_relative_${b}.html
+    ${pg_dir}plotgen --debug -i ./${b}/partial.tsv --drop-nan --apply-func div 100 --apply-icolumns : --palette bright --x-type category --y-tick-format ',.2%' --plot bar --file ./raw_data/partial_relative_${b}.tsv -o ./graphs/partial_relative_${b}.html
 done
 
 echo "DONE"
