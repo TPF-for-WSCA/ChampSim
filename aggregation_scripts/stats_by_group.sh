f7151ca375c12ff70b07a0d641cfc38098e3d786
Roman Kaspar Brunner <roman.k.brunner@ntnu.no>
2023-07-06 15:20:42 +0200

    handle division by 0 nans for spec benchmarks

--- aggregation_scripts/stats_by_group.sh
+++ aggregation_scripts/stats_by_group.sh
@@ -25,9 +25,9 @@ do
 
 	echo "Plotting ${b}"
     
-    ${pg_dir}plotgen --debug -i ./${b}/mpki.tsv --drop-nan --palette bright --add-column AVG --apply-func add --apply-icolumns :-1 --normalise-to-row $((2**15)) --apply-func sub 1 --apply-icolumns : --x-type category --y-tick-format ',.2%' --plot bar --file ./raw_data/mpki_relative_${b}.tsv -o ./graphs/mpki_relative_${b}.html
-    ${pg_dir}plotgen --debug -i ./${b}/ipc.tsv --drop-nan --palette bright --add-column AVG --apply-func add --apply-icolumns :-1 --normalise-to-row $((2**15)) --apply-func sub 1 --apply-icolumns : --x-type category --y-tick-format ',.2%' --plot bar --file ./raw_data/ipc_relative_${b}.tsv -o ./graphs/ipc_relative_${b}.html
-    ${pg_dir}plotgen --debug -i ./${b}/partial.tsv --drop-nan --apply-func div 100 --apply-icolumns : --palette bright --x-type category --y-tick-format ',.2%' --plot bar --file ./raw_data/partial_relative_${b}.tsv -o ./graphs/partial_relative_${b}.html
+    ${pg_dir}plotgen --debug -i ./${b}/mpki.tsv --drop-nan --palette bright --add-column AVG --apply-func add --apply-icolumns :-1 --normalise-to-row $((2**15)) --apply-func sub 1 --apply-icolumns : --apply-function cset = nan 0 --apply-icolumns : --x-type category --y-tick-format ',.2%' --plot bar --file ./raw_data/mpki_relative_${b}.tsv -o ./graphs/mpki_relative_${b}.html
+    ${pg_dir}plotgen --debug -i ./${b}/ipc.tsv --drop-nan --palette bright --add-column AVG --apply-func add --apply-icolumns :-1 --normalise-to-row $((2**15)) --apply-func sub 1 --apply-icolumns : --apply-function cset = nan 0 --apply-icolumns : --x-type category --y-tick-format ',.2%' --plot bar --file ./raw_data/ipc_relative_${b}.tsv -o ./graphs/ipc_relative_${b}.html
+    ${pg_dir}plotgen --debug -i ./${b}/partial.tsv --drop-nan --apply-func div 100 --apply-icolumns : --palette bright --apply-function cset = nan 0 --apply-icolumns : --x-type category --y-tick-format ',.2%' --plot bar --file ./raw_data/partial_relative_${b}.tsv -o ./graphs/partial_relative_${b}.html
 done
 
 echo "DONE"
