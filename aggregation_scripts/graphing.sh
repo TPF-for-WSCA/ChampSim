5a1fe6ba0aee39e74778967f97aa9450898d96a2
Roman Kaspar Brunner <roman.k.brunner@ntnu.no>
2023-08-07 14:36:29 +0200

    graph size

--- aggregation_scripts/graphing.sh
+++ aggregation_scripts/graphing.sh
@@ -1,8 +1,8 @@
 # IPC/MPKI
-export filename=ipc.tsv; export output_name='ipc_avg_bar'; plotgen -i ./${filename} --palette bright --add-column AVG --apply-func add --apply-icolumns :7 7: --print --normalise-to-irow 1 --apply-func sub 1 --apply-icolumns : --x-type category --y-tick-format ',.2%' --print --plot bar --line-mode lines+markers --file ./raw_data/${output_name}.tsv -o ./graphs/${output_name}.html --width 18cm --height 4cm -o ./graphs/${output_name}.pdf
+export filename=ipc.tsv; export output_name='ipc_avg_bar'; plotgen -i ./${filename} --palette bright --add-column AVG --apply-func add --apply-icolumns :7 7: --print --normalise-to-irow 1 --apply-func sub 1 --apply-icolumns : --x-type category --y-tick-format ',.2%' --print --plot bar --line-mode lines+markers --file ./raw_data/${output_name}.tsv -o ./graphs/${output_name}.html --width 1800 --height 400 -o ./graphs/${output_name}.pdf
 
 # Block and similar stats
-export filename=num_cl_with_block_size.tsv; export output_name='block_sizes_accum_avg_bar'; plotgen -i */${filename} --join index --no-columns --icolumn-names 1: --trace-names */${filename} --palette bright --add-column AVG --apply-func add --apply-icolumns :7 7: --apply-func cumsum --apply-columns : --print --normalise-function max --normalise-rows --print --plot bar --line-mode lines+markers --file ./raw_data/${output_name}.tsv -o ./graphs/${output_name}.html --width 18cm --height 4cm -o ./graphs/${output_name}.pdf
+export filename=num_cl_with_block_size.tsv; export output_name='block_sizes_accum_avg_bar'; plotgen -i */${filename} --join index --no-columns --icolumn-names 1: --trace-names */${filename} --palette bright --add-column AVG --apply-func add --apply-icolumns :7 7: --apply-func cumsum --apply-columns : --print --normalise-function max --normalise-rows --print --plot bar --line-mode lines+markers --file ./raw_data/${output_name}.tsv -o ./graphs/${output_name}.html --width 1800 --height 400 -o ./graphs/${output_name}.pdf
 
 # Some sort of naming but naming is somehow wrong when executed from bash
  export filename='cpu0_L1I_buffer_num_cl_with_block_size.tsv'; export
