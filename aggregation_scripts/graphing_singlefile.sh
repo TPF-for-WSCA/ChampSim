6b74e69bad8b3bcf96ea85381af4356d20413d94
Roman Kaspar Brunner <roman.k.brunner@ntnu.no>
2023-08-07 14:18:36 +0200

    fix for full width

--- aggregation_scripts/graphing_singlefile.sh
+++ aggregation_scripts/graphing_singlefile.sh
@@ -5,5 +5,5 @@ io_files=("ipc" "mpki" "partial")
 for f in ${io_files[@]}
 do
 	echo "Plotting ${f}.tsv"
-	plotgen --debug -i ./${f}.tsv --print --palette bright --add-column AVG --apply-func add --apply-icolumns :12 13: --apply-func div 13 --apply-icolumns 13 --print --normalise-to-irow 3 --apply-func sub 1 --apply-icolumns : --x-type category --y-tick-format ',.2%' --print --plot bar --line-mode lines+markers --file ./raw_data/${output_name}.tsv -o ./graphs/${output_name}.html -o ./graphs/${output_name}.pdf
+	plotgen --debug -i ./${f}.tsv --print --palette bright --add-column AVG --apply-func add --apply-icolumns :12 13: --apply-func div 13 --apply-icolumns 13 --print --normalise-to-irow 3 --apply-func sub 1 --apply-icolumns : --x-type category --y-tick-format ',.2%' --print --plot bar --line-mode lines+markers --file ./raw_data/${output_name}.tsv -o ./graphs/${output_name}.html --width 18cm --height 4cm -o ./graphs/${output_name}.pdf
 done
