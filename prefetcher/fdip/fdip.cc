8d94ace04fd6bfb4b1cbc38c1fd09a44919948e0
Roman Brunner <roman.brunner@siga.swiss>
2023-11-20 21:32:43 +0100

    prefetcher more realistic

--- prefetcher/fdip/fdip.cc
+++ prefetcher/fdip/fdip.cc
@@ -4,8 +4,8 @@
 
 #include "cache.h"
 #include "ooo_cpu.h"
-#define MAX_PFETCHQ_ENTRIES 128
-#define MAX_RECENT_PFETCH 32
+#define MAX_PFETCHQ_ENTRIES 64
+#define MAX_RECENT_PFETCH 10
 
 std::deque<std::tuple<uint64_t, uint64_t, uint8_t>> prefetch_queue; // Storage: 64-bits * 48 (queue size) = 384 bytes
 std::deque<uint64_t> recent_prefetches;                             // Storage: 64-bits * 10 (queue size) = 80 bytes
@@ -39,6 +39,10 @@ uint32_t O3_CPU::prefetcher_cache_operate(uint64_t v_addr, uint8_t cache_hit, ui
   if ((cache_hit == 0) && (L1I->get_occupancy(0, 0) < L1I->MSHR_SIZE >> 1)) {
     uint64_t pf_addr = v_addr + (1 << LOG2_BLOCK_SIZE);
     prefetch_code_line(pf_addr);
+    recent_prefetches.push_back(pf_addr);
+    if (recent_prefetches.size() > MAX_RECENT_PFETCH) {
+      recent_prefetches.pop_front();
+    }
   }
   return metadata_in;
 }
@@ -46,6 +50,7 @@ uint32_t O3_CPU::prefetcher_cache_operate(uint64_t v_addr, uint8_t cache_hit, ui
 void O3_CPU::prefetcher_cycle_operate()
 {
   while (prefetch_queue.size() && L1I->get_occupancy(0, 0) < L1I->MSHR_SIZE >> 1) {
+
 #define L1I (static_cast<CACHE*>(L1I_bus.lower_level))
     auto it = std::find(recent_prefetches.begin(), recent_prefetches.end(), std::get<0>(prefetch_queue.front()));
     if (L1I->hit_test(std::get<1>(prefetch_queue.front()), std::get<2>(prefetch_queue.front()))) {
@@ -56,7 +61,8 @@ void O3_CPU::prefetcher_cycle_operate()
       prefetch_code_line(std::get<0>(prefetch_queue.front()));
       recent_prefetches.push_back(std::get<0>(prefetch_queue.front()));
     } else {
-      std::cout << "recent prefetch skip" << std::endl;
+      std::cout << "recent prefetch skip -- something failed when inserting into the pfq" << std::endl;
+      continue; // note: if all goes well we never end up here...
     }
     if (recent_prefetches.size() > MAX_RECENT_PFETCH) {
       recent_prefetches.pop_front();
