787f3b0c88e13e3dfdcb95070fb4d8a3a0243e6d
Roman Brunner <roman.brunner@siga.swiss>
2023-12-04 17:30:04 +0100

    extended basic btb

--- btb/extended_basic_btb/extended_basic_btb.cc
+++ btb/extended_basic_btb/extended_basic_btb.cc
@@ -14,7 +14,7 @@
 #define BASIC_BTB_RAS_SIZE 64
 #define BASIC_BTB_CALL_INSTR_SIZE_TRACKERS 1024
 #define BASIC_BTB_BRANCH_TABLE 131072
-#define EXTENDED_BTB_MAX_LOOP_BRANCH 256
+#define EXTENDED_BTB_MAX_LOOP_BRANCH 128
 
 struct BRANCH_TABLE_ENTRY {
   uint8_t branch_type;
@@ -226,7 +226,8 @@ bool O3_CPU::is_block_ending_branch(uint64_t ip)
 void O3_CPU::update_btb(uint64_t ip, uint64_t branch_target, uint8_t taken, uint8_t branch_type)
 {
   if (branch_type != NOT_BRANCH && not BLOCK_ENDING_BRANCH(branch_type))
-    branch_table[(ip >> 2)] = {branch_type, 4, (branch_target < ip && (ip - branch_target) < EXTENDED_BTB_MAX_LOOP_BRANCH) ? true : false};
+    branch_table[(ip >> 2)] = {
+        branch_type, 4, (branch_target < ip) ? true : false}; //&& (ip - branch_target) < EXTENDED_BTB_MAX_LOOP_BRANCH  // no influence on distance measurement
   // updates for indirect branches
   if ((branch_type == BRANCH_INDIRECT) || (branch_type == BRANCH_INDIRECT_CALL)) {
     basic_btb_indirect[cpu][basic_btb_indirect_hash(cpu, ip)] = branch_target;
