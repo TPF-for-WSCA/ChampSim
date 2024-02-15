ff8ee1d638ff6d26a9160e0a1c427e3d97272167
Roman Brunner <roman.brunner@siga.swiss>
2024-02-15 09:31:36 +0100

    build 16k

--- saga_build.sh
+++ saga_build.sh
@@ -6,9 +6,9 @@
 #build_configs=("saga_vcl_buffer_64d_max_way_config.json" "saga_vcl_buffer_8f_config.json" "saga_vcl_buffer_16f_config.json" "saga_vcl_buffer_32f_config.json" "saga_vcl_buffer_64f_config.json" "saga_vcl_buffer_128f_config.json") 
 #build_configs=("saga_32k_config.json" "saga_vcl_buffer_8f_config.json" "saga_vcl_buffer_4f_config.json" "saga_vcl_buffer_128f_config.json" "saga_vcl_buffer_64f_config.json" "saga_vcl_buffer_32f_config.json" "saga_vcl_buffer_16f_config.json" "saga_vcl_buffer_256f_config.json") 
 #build_configs=("saga_32k_config.json" "saga_32k_fdip_config.json" "saga_32k_fdip_btb2048_config.json" "saga_32k_fdip_btb8192_config.json" "saga_vcl_buffer_64d_fdip_config.json" "saga_vcl_buffer_64d_fdip_btb2048_config.json" "saga_vcl_buffer_64d_fdip_btb8192_config.json" "saga_vcl_buffer_64d_config.json") 
-build_configs=("saga_ubs_10_small_ways.json" "saga_ubs_10_small_ways_not_extending.json" "saga_ubs_10_ways.json" "saga_ubs_10_ways_not_extending.json" "saga_ubs_12_small_ways.json" "saga_ubs_12_small_ways_not_extending.json" "saga_ubs_14_small_ways.json" "saga_ubs_14_small_ways_not_extending.json")
+#build_configs=("saga_ubs_10_small_ways.json" "saga_ubs_10_small_ways_not_extending.json" "saga_ubs_10_ways.json" "saga_ubs_10_ways_not_extending.json" "saga_ubs_12_small_ways.json" "saga_ubs_12_small_ways_not_extending.json" "saga_ubs_14_small_ways.json" "saga_ubs_14_small_ways_not_extending.json")
 #build_configs=("saga_4k_data_config.json" "saga_8k_data_config.json" "saga_16k_data_config.json" "saga_32k_data_config.json" "saga_64k_data_config.json" "saga_128k_data_config.json" "saga_256k_data_config.json") 
-
+build_configs=("saga_16k_config.json")
 old_dir=$(pwd)
 cd ~/workspace/ChampSim/
 for build_script in ${build_configs[@]}
