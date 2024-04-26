c137e917fd8fa9fc09ebe9a7e0f8259b7e609a79
Roman Kaspar Brunner <roman.k.brunner@ntnu.no>
2024-04-26 13:37:49 +0200

    update to correct offset calculation for all stats

--- btb/conv_btb/conv_btb.cc
+++ btb/conv_btb/conv_btb.cc
@@ -363,21 +363,24 @@ void O3_CPU::update_btb(uint64_t ip, uint64_t branch_target, uint8_t taken, uint
       num_bits = 0;
     } else {
       uint64_t diff_bits = (branch_target >> 2) ^ (ip >> 2);
+      uint64_t offset = branch_target & (diff_bits - 1);
       num_bits = 0;
       while (diff_bits != 0) {
         diff_bits = diff_bits >> 1;
         num_bits++;
       }
+      assert(num_bits <= 64);
       extern uint8_t knob_collect_offsets;
-      if (knob_collect_offsets && num_bits < 4) {
-        pc_offset_pairs.push_back(std::make_pair(ip, (branch_target >> 2) - (ip >> 2)));
-        auto idx = (branch_target > ip) ? (branch_target >> 2) - (ip >> 2) + 64 : (ip >> 2) - (branch_target >> 2);
-        assert(idx <= 128);
-        offset_counts[idx]++;
-        for (uint64_t i = 0; i < 64; i++) {
-          uint64_t bitsel = 0x1ull << i;
-          if (bitsel & ip)
-            pc_bits_offset[idx][i]++;
+      if (knob_collect_offsets) {
+        pc_offset_pairs.push_back(std::make_pair(ip, offset));
+        if (num_bits < 6) {
+          assert(idx <= 128);
+          offset_counts[offset]++;
+          for (uint64_t i = 0; i < 64; i++) {
+            uint64_t bitsel = 0x1ull << i;
+            if (bitsel & ip)
+              pc_bits_offset[offset][i]++;
+          }
         }
       }
     }
