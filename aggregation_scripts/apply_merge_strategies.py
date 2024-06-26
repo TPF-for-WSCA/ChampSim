296680a00f6b6ce3b3695f87216e88299dd8b0f2
Roman Brunner <roman.brunner@siga.swiss>
2024-06-26 19:32:13 +0200

    quick and dirty hack

--- aggregation_scripts/apply_merge_strategies.py
+++ aggregation_scripts/apply_merge_strategies.py
@@ -455,7 +455,9 @@ def apply_storage_efficiency_analysis(
     max_efficiency = 0.0
     min_efficiency = 1.0
     for useful_bytes in get_uint64_t_from_tracefile(tracefile_path):
-        assert useful_bytes <= total_cache_size
+        # assert useful_bytes <= total_cache_size
+        if useful_bytes > total_cache_size:
+            continue
 
         if useful_bytes == 0:
             print("all useless...")
