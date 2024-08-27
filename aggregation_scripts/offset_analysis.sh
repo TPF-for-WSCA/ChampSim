6e012f1fb1a84f0f888638222470d864c6c26d77
Roman Kaspar Brunner <roman.k.brunner@ntnu.no>
2024-08-27 14:18:50 +0200

    offset_ananlysi

--- aggregation_scripts/offset_analysis.sh
+++ aggregation_scripts/offset_analysis.sh
@@ -12,7 +12,7 @@
 module load matplotlib/3.8.2-gfbf-2023b
 benchmarks=("ipc1_server" "ipc1_client" "ipc1_spec")
 #benchmarks=("whiskey" "merced" "delta" "charlie")
-baseline_config=["sizes_offset_btbx"]
+baseline_config=("sizes_1k_btb" "sizes_2k_btb" "sizes_4k_btb" "sizes_8k_btb" "sizes_16k_btb" "sizes_32k_btb")
 
 old_dir=pwd
 
