f1aef5bd22b1e78040b66eef9bc56310f2a3174a
Roman Brunner <roman.brunner@siga.swiss>
2024-11-03 13:31:07 -0600

    correct set of workloads

--- aggregation_scripts/stats_by_group.sh
+++ aggregation_scripts/stats_by_group.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #benchmarks=("tanvir")
-benchmarks=("google_charlie" "google_whiskey" "google_delta" "google_merced")
+benchmarks=("ipc1_server" "ipc1_client" "ipc1_spec")
 #benchmarks=("crc2_spec" "crc2_cloud" "dpc3")
 pg_dir=""
 
