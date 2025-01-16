3ff9dcc86547647321d71e706cf267106ebf8181
Roman K. Brunner <roman.k.brunner@ntnu.no>
2025-01-16 15:21:30 +0100

    json_data fixed

--- aggregation_scripts/json_data.py
+++ aggregation_scripts/json_data.py
@@ -2,12 +2,16 @@ import json
 import os
 import sys
 
-regions = {}
+regions = {100: {}, 99.5: {}, 99: {}, 95: {}, 90: {}}
 
 assert(len(sys.argv) == 2)
 for dir in os.listdir(sys.argv[1]):
+    if not os.path.isdir(os.path.join(sys.argv[1], dir)):
+        continue
+    if dir.startswith(".") or ".tsv" in dir or ".txt" in dir:
+        continue
     json_data = None
-    with open(os.path.join(sys.argv[1], dir, stats.json)) as f:
+    with open(os.path.join(sys.argv[1], dir, "stats.json")) as f:
         json_data = json.load(f)
     regions_data = json_data[0]['roi']['cores'][0]['regions_covered']
     regions[100][dir] = regions_data['100%']
@@ -18,11 +22,23 @@ for dir in os.listdir(sys.argv[1]):
 
 for percentage in [100, 99.5, 99, 95, 90]:
     outname = os.path.join(sys.argv[1], f"{percentage}_region_sampling.tsv")
+    max_idx = 0
     with open(outname, "w+") as outfile:
-        headers = regions[percentage].keys()
+        headers = list(regions[percentage].keys())
         for header in headers:
             outfile.write(f"\t{header}")
+            max_idx = max(len(regions[percentage][header]), max_idx)
         outfile.write("\n")
-        for header in headers:
-            outfile.write(f"\t{regions[percentage][header]}")
-        outfile.write("\n")
+        # TODO: loop over single entries in regions, one at a time for all headers and add them
+        idx = 0
+        while(1):
+            for header in headers:
+                if (len(regions[percentage][header]) <= idx):
+                    outfile.write("\t''")
+                    continue
+                outfile.write(f"\t'{regions[percentage][header][idx]}'")
+            outfile.write("\n")
+            idx += 1
+            if idx == max_idx:
+                break
+
