df31a3cf467dc47d7a686e38c112bb7dc0050640
Roman Kaspar Brunner <roman.k.brunner@ntnu.no>
2025-01-27 10:41:53 +0100

    faster and 20th run

--- idun_runner.sh
+++ idun_runner.sh
@@ -22,8 +22,8 @@ module load GCCcore/12.2.0
 #binaries=("champsim32k_base_btb" "champsim32k_base_btbx" "champsim32k_hash_btbx" "champsim32k_perfect_l1i" "champsim32k_perfect_btb")
 #binary_dir=("btb_region_tag" "btb_512_region_tag_exp" "btb_1k_region_tag_exp" "btb_2k_region_tag_exp" "btb_4k_region_tag_exp" "btb_8k_region_tag_exp")
 binary_dir=("btb_region_tag")
-count=19
-timelimit="6:00:00"
+count=20
+timelimit="2:30:00"
 #binary_dir=("size_sensitivity")
 #binaries=("ubs" "ubs_unaligned" "ubs_extended" "ubs_unaligned_extended")
 #binaries=("ubs_overhead_isca_extend_lru" "ubs_overhead_isca_lru")
