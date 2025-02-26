a9fc1222f56d1bd0231e6fa72589013425576eb2
Roman Kaspar Brunner <roman.k.brunner@ntnu.no>
2025-02-26 10:53:47 +0100

    idun not perfect config

--- btb/xbtbx/xbtbx.cc
+++ btb/xbtbx/xbtbx.cc
@@ -19,7 +19,7 @@
 #include "ooo_cpu.h"
 
 #define FULLY_ASSOCIATIVE_REGIONS true // if this is false it is direct mapped region btb
-#define PERFECT_MAPPING true
+#define PERFECT_MAPPING false
 #define SMALL_BIG_WAY_SPLIT 14
 #define SAMPLING_DISTANCE 1000000
 
