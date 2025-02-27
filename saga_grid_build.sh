e49d67c98277ca4fe905ba25642c9b22379833b9
Roman Brunner <roman.brunner@siga.swiss>
2025-02-27 16:58:06 +0100

    grid build only old configs

--- saga_grid_build.sh
+++ saga_grid_build.sh
@@ -5,8 +5,10 @@
 #build_configs=("saga_vcl_buffer_64d_config.json" "saga_64k_config.json" "saga_32k_config.json" "saga_128m_config.json" "saga_vcl_buffer_16d_config.json" "saga_vcl_buffer_16a_config.json" "saga_vcl_buffer_64d_arm_config.json" "saga_vcl_buffer_64d_config.json" "saga_vcl_buffer_16d_config.json" "saga_vcl_buffer_16a_config.json" ) 
 #build_configs=("saga_vcl_buffer_64d_max_way_config.json" "saga_vcl_buffer_8f_config.json" "saga_vcl_buffer_16f_config.json" "saga_vcl_buffer_32f_config.json" "saga_vcl_buffer_64f_config.json" "saga_vcl_buffer_128f_config.json") 
 #build_configs=("saga_32k_config.json" "saga_vcl_buffer_8f_config.json" "saga_vcl_buffer_4f_config.json" "saga_vcl_buffer_128f_config.json" "saga_vcl_buffer_64f_config.json" "saga_vcl_buffer_32f_config.json" "saga_vcl_buffer_16f_config.json" "saga_vcl_buffer_256f_config.json") 
-#build_configs=("saga_4k_data_config.json" "saga_8k_data_config.json" "saga_16k_data_config.json" "saga_32k_data_config.json" "saga_64k_data_config.json" "saga_128k_data_config.json" "saga_256k_data_config.json") 
-build_dir=("btb_4k_region_tag_set_associative_set_idx")
+#build_configs=("saga_4k_data_config.json" "saga_8k_data_config.json" "saga_16k_data_config.json" "saga_32k_data_config.json" "saga_64k_data_config.json" "saga_128k_data_config.json" "saga_256k_data_config.json")
+# build_dir=("btb_1k_region_tag_exp" "btb_4k_region_tag_exp")
+# build_dir=("btb_4k_region_tag_set_associative_set_idx")
+build_dir=("btb_4k_region_tag_exp")
 
 old_dir=$(pwd)
 cd /cluster/projects/nn4650k/workspace/ChampSim/
