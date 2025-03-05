d007b08539c4f7b6f31c1598efca41c61969bc75
Roman Kaspar Brunner <roman.k.brunner@ntnu.no>
2025-03-05 14:39:22 +0100

    also generate full graph

--- aggregation_scripts/json_data.py
+++ aggregation_scripts/json_data.py
@@ -12,6 +12,14 @@ regions = {
     90: defaultdict(dict),
 }
 
+all_ways = {
+    100: {},
+    99.5: {},
+    99: {},
+    95: {},
+    90: {},
+}
+
 assert len(sys.argv) == 2
 for dir in os.listdir(sys.argv[1]):
     if not os.path.isdir(os.path.join(sys.argv[1], dir)):
@@ -28,8 +36,33 @@ for dir in os.listdir(sys.argv[1]):
         regions[99][int(way)][dir] = regions_data[way]["99%"]
         regions[95][int(way)][dir] = regions_data[way]["95%"]
         regions[90][int(way)][dir] = regions_data[way]["90%"]
+        all_ways[100][dir] += regions_data[way]["100%"]
+        all_ways[99.5][dir] += regions_data[way]["99.5%"]
+        all_ways[99][dir] += regions_data[way]["99%"]
+        all_ways[95][dir] += regions_data[way]["95%"]
+        all_ways[90][dir] += regions_data[way]["90%"]
 
 for percentage in [100, 99.5, 99, 95, 90]:
+    outname = os.path.join(sys.argv[1], "overall_region_sampling.tsv")
+    max_idx = 0
+    with open(outname, "w+") as outfile:
+        headers = list(all_ways[percentage].keys())
+        for header in headers:
+            outfile.write(f"\t{header}")
+            max_idx = max(len(all_ways[percentage][header]), max_idx)
+        outfile.write("\n")
+        # TODO: loop over single entries in regions, one at a time for all headers and add them
+        idx = 0
+        while 1:
+            for header in headers:
+                if len(all_ways[percentage][header]) <= idx:
+                    outfile.write("\t")
+                    continue
+                outfile.write(f"\t{all_ways[percentage][header][idx]}")
+            outfile.write("\n")
+            idx += 1
+            if idx == max_idx:
+                break
     for way in regions[percentage].keys():
         outname = os.path.join(
             sys.argv[1], f"{percentage}_way_{way}_region_sampling.tsv"
