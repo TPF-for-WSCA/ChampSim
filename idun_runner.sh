cde9c821ac6159c0cc1aeb648dfc06822d2de62c
Roman Kaspar Brunner <roman.k.brunner@ntnu.no>
2025-03-05 16:04:15 +0100

    rerun lost data

--- idun_runner.sh
+++ idun_runner.sh
@@ -21,7 +21,7 @@ module load GCCcore/12.2.0
 #binaries=("champsim32k" "ubs_12_ways_not_extending" "ubs_12_ways" "ubs_14_ways" "ubs_14_small_ways" "ubs_12_small_ways_not_extending" "ubs_12_small_ways" "ubs_14_small_ways_not_extending" "ubs_14_small_ways" "ubs_16_ways_precise" "ubs_16_ways_precise_not_extending")
 #binaries=("champsim32k_base_btb" "champsim32k_base_btbx" "champsim32k_hash_btbx" "champsim32k_perfect_l1i" "champsim32k_perfect_btb")
 # binary_dir=("btb_region_tag" "btb_4k_region_tag_split_exp" "btb_512_region_tag_exp" "btb_256_region_tag_exp" "btb_1k_region_tag_exp" "btb_2k_region_tag_exp" "btb_4k_region_tag_exp" "btb_8k_region_tag_exp")
-binary_dir=("btb_region_tag")
+binary_dir=("btb_4k_region_tag_split_exp")
 suffix="dm_np"
 count=0
 timelimit="2:30:00"
