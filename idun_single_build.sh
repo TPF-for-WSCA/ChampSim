12698368c5056d72c50162aa0d5e163e23eeb2f1
Roman Brunner <roman.brunner@siga.swiss>
2025-05-05 09:15:01 +0200

    same size 12b configs just for comparison

---
+++ idun_single_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+build_configs=("4k_max_region_configurations/btbx_tag_12_constant_size_pluss_2.json" "IDUN_CONFIGS/4k_max_region_configurations/btbx_tag_12_constant_size.json") #  "btb_512_region_tag_exp" "btb_1k_region_tag_exp" "btb_2k_region_tag_exp" "btb_4k_region_tag_exp" "btb_8k_region_tag_exp" "btb_256_region_tag_exp")
+# build_dir=("btb_512_region_tag_exp" "btb_1k_region_tag_exp" "btb_2k_region_tag_exp" "btb_4k_region_tag_exp" "btb_8k_region_tag_exp")
+
+old_dir=$(pwd)
+cd ~/ChampSim/
+for spec_dir in ${build_dir[@]}
+do
+    echo "Building experiment $spec_dir"
+    echo -e "\tConfiguring ./IDUN_CONFIGS/${build_script}"
+    ./config.sh ./IDUN_CONFIGS/$build_script
+    echo -e "\tBuilding ./IDUN_CONFIGS/${build_script}"
+    make -j &>> /cluster/work/romankb/build_$(basename ${build_script%.json}).log
+done
+cd $old_dir
