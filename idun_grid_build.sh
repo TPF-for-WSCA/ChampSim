5da32e039a79f0bef50de5f65fdb12e420882327
Roman Kaspar Brunner <roman.k.brunner@ntnu.no>
2025-05-06 13:56:42 +0200

    small scale run/example

--- idun_grid_build.sh
+++ idun_grid_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-build_dir=("4k_max_region_configurations_tage_scl") #  "btb_512_region_tag_exp" "btb_1k_region_tag_exp" "btb_2k_region_tag_exp" "btb_4k_region_tag_exp" "btb_8k_region_tag_exp" "btb_256_region_tag_exp")
+build_dir=("4k_max_region_configurations") #  "btb_512_region_tag_exp" "btb_1k_region_tag_exp" "btb_2k_region_tag_exp" "btb_4k_region_tag_exp" "btb_8k_region_tag_exp" "btb_256_region_tag_exp")
 # build_dir=("btb_512_region_tag_exp" "btb_1k_region_tag_exp" "btb_2k_region_tag_exp" "btb_4k_region_tag_exp" "btb_8k_region_tag_exp")
 
 old_dir=$(pwd)
