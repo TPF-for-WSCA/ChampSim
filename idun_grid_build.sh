dd0477de6b7de37b2f525e526937b575b6bf35d6
Roman Brunner <roman.brunner@siga.swiss>
2025-05-28 22:39:17 +0200

    aliasing corrected

--- idun_grid_build.sh
+++ idun_grid_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-build_dir=("aliasing_vs_entries_2k_to_4k") # "4k_12b_region_tag_sensitivity" "4k_10b_region_tag_sensitivity") #  "btb_512_region_tag_exp" "btb_1k_region_tag_exp" "btb_2k_region_tag_exp" "btb_4k_region_tag_exp" "btb_8k_region_tag_exp" "btb_256_region_tag_exp")
+build_dir=("aliasing_vs_entries") # "4k_12b_region_tag_sensitivity" "4k_10b_region_tag_sensitivity") #  "btb_512_region_tag_exp" "btb_1k_region_tag_exp" "btb_2k_region_tag_exp" "btb_4k_region_tag_exp" "btb_8k_region_tag_exp" "btb_256_region_tag_exp")
 # build_dir=("btb_512_region_tag_exp" "btb_1k_region_tag_exp" "btb_2k_region_tag_exp" "btb_4k_region_tag_exp" "btb_8k_region_tag_exp")
 
 old_dir=$(pwd)
