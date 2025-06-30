020a552acc95c80b03f4dc82452db0acb4b90478
Roman K. Brunner <roman.k.brunner@ntnu.no>
2025-06-30 11:11:54 +0200

    small changes

--- idun_runner.sh
+++ idun_runner.sh
@@ -22,11 +22,11 @@ module load GCCcore/12.2.0
 #binaries=("champsim32k_base_btb" "champsim32k_base_btbx" "champsim32k_hash_btbx" "champsim32k_perfect_l1i" "champsim32k_perfect_btb")
 # binary_dir=("btb_region_tag" "btb_4k_region_tag_split_exp" "btb_512_region_tag_exp" "btb_256_region_tag_exp" "btb_1k_region_tag_exp" "btb_2k_region_tag_exp" "btb_4k_region_tag_exp" "btb_8k_region_tag_exp")
 suffix=""
-binary_dir=("aliasing_vs_entries") # "btb_4k_10b_tag_sensitivity" "btb_4k_12b_tag_sensitivity" )
+binary_dir=("btb_efficiency_point_optimisations") # "btb_4k_10b_tag_sensitivity" "btb_4k_12b_tag_sensitivity" )
 count=0
-timelimit="1:30:00"
-warmup=30000000
-simulation=30000000
+timelimit="2:30:00"
+warmup=50000000
+simulation=50000000
 #binary_dir=("size_sensitivity")
 #binaries=("ubs" "ubs_unaligned" "ubs_extended" "ubs_unaligned_extended")
 #binaries=("ubs_overhead_isca_extend_lru" "ubs_overhead_isca_lru")
