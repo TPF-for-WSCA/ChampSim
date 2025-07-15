1855ea56b3d89290e344acff00ccadb5f468249b
Roman Brunner <roman.brunner@siga.swiss>
2025-07-15 12:06:04 +0200

    fixed configs

--- saga_grid_build.sh
+++ saga_grid_build.sh
@@ -9,7 +9,7 @@
 # build_dir=("btb_1k_region_tag_exp" "btb_4k_region_tag_exp")
 # build_dir=("btb_4k_region_tag_set_associative_set_idx")
 # build_dir=("small_set_configurations")
-build_dir=("equiperformance_configs")
+build_dir=("region_based_optimisation")
 
 old_dir=$(pwd)
 cd /cluster/projects/nn4650k/workspace/ChampSim/
