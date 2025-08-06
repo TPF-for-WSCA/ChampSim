3401d75e9690d3db3b99beccc3c27803346ace2d
Roman K. Brunner <roman.k.brunner@ntnu.no>
2025-08-06 16:59:02 +0200

    all region no tag experiment

--- idun_grid_build.sh
+++ idun_grid_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-build_dir=("default_full_btb_grid_search") # "4k_12b_region_tag_sensitivity" "4k_10b_region_tag_sensitivity") #  "btb_512_region_tag_exp" "btb_1k_region_tag_exp" "btb_2k_region_tag_exp" "btb_4k_region_tag_exp" "btb_8k_region_tag_exp" "btb_256_region_tag_exp")
+build_dir=("btb_8k_all_region_no_tag") # "4k_12b_region_tag_sensitivity" "4k_10b_region_tag_sensitivity") #  "btb_512_region_tag_exp" "btb_1k_region_tag_exp" "btb_2k_region_tag_exp" "btb_4k_region_tag_exp" "btb_8k_region_tag_exp" "btb_256_region_tag_exp")
 # build_dir=("btb_512_region_tag_exp" "btb_1k_region_tag_exp" "btb_2k_region_tag_exp" "btb_4k_region_tag_exp" "btb_8k_region_tag_exp")
 
 old_dir=$(pwd)
