9fb5896432a6827912dd049e69d97fbb96851a25
Roman K. Brunner <roman.k.brunner@ntnu.no>
2025-10-16 09:26:55 +0200

    got all running on IDUN

--- idun_grid_build.sh
+++ idun_grid_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-build_dir=("way_set_exp") # "4k_12b_region_tag_sensitivity" "4k_10b_region_tag_sensitivity") #  "btb_512_region_tag_exp" "btb_1k_region_tag_exp" "btb_2k_region_tag_exp" "btb_4k_region_tag_exp" "btb_8k_region_tag_exp" "btb_256_region_tag_exp")
+build_dir=("equiperformance_configs") # "4k_12b_region_tag_sensitivity" "4k_10b_region_tag_sensitivity") #  "btb_512_region_tag_exp" "btb_1k_region_tag_exp" "btb_2k_region_tag_exp" "btb_4k_region_tag_exp" "btb_8k_region_tag_exp" "btb_256_region_tag_exp")
 # build_dir=("btb_512_region_tag_exp" "btb_1k_region_tag_exp" "btb_2k_region_tag_exp" "btb_4k_region_tag_exp" "btb_8k_region_tag_exp")
 
 old_dir=$(pwd)
